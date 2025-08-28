ece0f3755fe0f368e3b34127978f0d3c921a7f10
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-08-28 13:52:58 -0300

    build: add Spring Boot base project with essential dependencies
    - Spring Web for REST API endpoints
    - Spring Data JPA for database operations
    - PostgreSQL Driver for database connection
    - Validation for input validation

---
+++ src/main/java/com/github/pblcnr/seraquepaguei/SeraQuePagueiApplication.java
@@ -0,0 +1,13 @@
+package com.github.pblcnr.seraquepaguei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeraQuePagueiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeraQuePagueiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pblcnr/seraquepaguei/SeraQuePagueiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pblcnr.seraquepaguei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeraQuePagueiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
