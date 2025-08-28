0a643caa8b5a8e9d95fcb8b7ab4c47ae592a7729
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-08-28 17:33:44 -0300

    feat: create UserRepository with email queries

---
+++ src/main/java/com/github/pblcnr/seraquepaguei/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.github.pblcnr.seraquepaguei.repository;
+
+import com.github.pblcnr.seraquepaguei.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
