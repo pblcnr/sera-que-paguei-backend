90e34be7d41fb00170b6e71f364690b7c8c9127d
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-08-29 23:07:44 -0300

    feat: create UserRequestDTO with validations

---
+++ src/main/java/com/github/pblcnr/seraquepaguei/dto/user/UserRequestDTO.java
@@ -0,0 +1,51 @@
+package com.github.pblcnr.seraquepaguei.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class UserRequestDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+    public UserRequestDTO() {
+
+    }
+
+    public UserRequestDTO(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
