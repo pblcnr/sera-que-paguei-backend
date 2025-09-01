e76bf314fdf019b7f5d3c8b4b923619281aa67de
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-09-01 14:09:29 -0300

    refactor: remove deprecated non-DTO methods from UserService

--- src/main/java/com/github/pblcnr/seraquepaguei/service/UserService.java
+++ src/main/java/com/github/pblcnr/seraquepaguei/service/UserService.java
@@ -18,31 +18,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser(User user) {
-
-        if (userRepository.existsByEmail(user.getEmail())) {
-            throw new RuntimeException("Email já cadastrado!");
-        }
-
-        // TODO: Hashear senha antes de salvar
-        // user.setSenha(passwordEncoder.encode(user.getSenha()));
-
-        return userRepository.save(user);
-
-    }
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
-    }
-
-    public Optional<User> getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
-
     public User updateUser(Long id, User userDetails) {
         User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 
